80539f63d2260d8d8f9facfe062259e567aad5a4
Cole Gottdank <colegottdank@gmail.com>
2021-08-21 17:54:00 -0400

    Implement justinas/alice package to better chain handlers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,8 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
+
 	mux := http.NewServeMux()     // create new routing thing
 	mux.HandleFunc("/", app.home) // add routes
 	mux.HandleFunc("/snippet", app.showSnippet)
@@ -11,5 +17,5 @@ func (app *application) routes() http.Handler {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))         // get files in static location
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer)) // StripPrefix for safety. Register new handler with ServerMux
 
-	return app.recoverPanic(app.logRequest(secureHeaders(mux)))
+	return standardMiddleware.Then(mux)
 }
