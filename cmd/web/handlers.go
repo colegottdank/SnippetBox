c06ad7dce75c3815a9fc94dcbda8f56422758067
Cole Gottdank <colegottdank@gmail.com>
2021-08-22 21:01:46 -0400

    Implement create snippet form

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -11,12 +11,6 @@ import (
 
 // home is a function/handler that is called when directed to /
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	// only route / path to home
-	if r.URL.Path != "/" {
-		app.notFound(w)
-		return
-	}
-
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -30,7 +24,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 // home is a function/handler that is called when directed to /snippet
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
 		return
@@ -51,14 +45,12 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
+	app.render(w, r, "create.page.tmpl", nil)
+}
+
 // home is a function/handler that is called when directed to /snippet/create
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.Header().Set("Allow", http.MethodPost)
-		app.clientError(w, http.StatusMethodNotAllowed)
-		return
-	}
-
 	title := "0 snail"
 	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
 	expires := "7"
@@ -69,5 +61,5 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,19 +3,21 @@ package main
 import (
 	"net/http"
 
+	"github.com/bmizerany/pat"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
 	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	mux := http.NewServeMux()     // create new routing thing
-	mux.HandleFunc("/", app.home) // add routes
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
+	mux := pat.New() // create new routing thing
+	mux.Get("/", http.HandlerFunc(app.home))
+	mux.Get("/snippet/create", http.HandlerFunc(app.createSnippetForm))
+	mux.Post("/snippet/create", http.HandlerFunc(app.createSnippet))
+	mux.Get("/snippet/:id", http.HandlerFunc(app.showSnippet))
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))         // get files in static location
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer)) // StripPrefix for safety. Register new handler with ServerMux
+	fileServer := http.FileServer(http.Dir("./ui/static/"))      // get files in static location
+	mux.Get("/static/", http.StripPrefix("/static", fileServer)) // StripPrefix for safety. Register new handler with ServerMux
 
 	return standardMiddleware.Then(mux)
 }
