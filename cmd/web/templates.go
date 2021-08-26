e0d1a23941a850358ae4b9aaac1fdaa20944940b
Cole Gottdank <colegottdank@gmail.com>
2021-08-25 21:52:05 -0400

    Implement errors.go and forms.go to handle form validation and errors better

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,17 +2,16 @@ package main
 
 import (
 	"html/template"
-	"net/url"
 	"path/filepath"
 	"time"
 
+	"colegottdank.com/snippetbox/pkg/forms"
 	"colegottdank.com/snippetbox/pkg/models"
 )
 
 type templateData struct {
 	CurrentYear int
-	FormData    url.Values
-	FormErrors  map[string]string
+	Form        *forms.Form
 	Snippet     *models.Snippet
 	Snippets    []*models.Snippet
 }
