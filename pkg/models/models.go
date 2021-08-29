ba3bef34fd1984c0401868ec9ecd1c46f119d40e
Cole Gottdank <colegottdank@gmail.com>
2021-08-29 14:18:22 -0400

    Add new routes for user signup, login and signout. Implement user models and mysql commands

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -5,7 +5,13 @@ import (
 	"time"
 )
 
-var ErrorNoRecord = errors.New("models: no matching record found")
+var (
+	ErrorNoRecord = errors.New("models: no matching record found")
+
+	ErrInvalidCredentials = errors.New("models: invalid credentials")
+
+	ErrDuplicateEmail = errors.New("models: duplicate email")
+)
 
 type Snippet struct {
 	ID      int
@@ -13,4 +19,13 @@ type Snippet struct {
 	Content string
 	Created time.Time
 	Expires time.Time
+}
+
+type User struct {
+	ID       int
+	name     string
+	email    string
+	password []byte
+	created  time.Time
+	active   bool
 }
